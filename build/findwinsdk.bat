4e00aab75e7256303c4dc15bc821b88487df117a
Samantha McVey <samantham@posteo.net>
2017-09-29 21:49:52 -0700

    more work trying to get SDK found

---
+++ build/findwinsdk.bat
@@ -0,0 +1,43 @@
+@ECHO OFF
+
+IF "%WindowsSdkVersion%"=="" (
+  CALL :SetWindowsSdkVersionHelper HKCU > nul 2>&1
+  IF ERRORLEVEL 1 CALL :SetWindowsSdkVersionHelper HKLM > nul 2>&1
+  IF ERRORLEVEL 1 GOTO ERROR_NOWSDK
+)
+
+CALL :SetWindowsSdkPathHelper > nul 2>&1
+IF ERRORLEVEL 1 GOTO ERROR_NOWSDK
+GOTO END
+
+:SetWindowsSdkPathHelper
+SET WindowsSdkPath=
+FOR /F "tokens=1,2*" %%i in ('REG QUERY "HKLM\SOFTWARE\Microsoft\Microsoft SDKs\Windows\%WindowsSdkVersion%" /V InstallationFolder') DO (
+    IF "%%i"=="InstallationFolder" (
+        SET "WindowsSdkPath=%%k"
+    )
+)
+IF "%WindowsSdkPath%"=="" EXIT /B 1
+EXIT /B 0
+
+:SetWindowsSdkVersion
+CALL :GetWindowsSdkVersionHelper HKCU > nul 2>&1
+IF ERRORLEVEL 1 CALL :GetWindowsSdkVersionHelper HKLM > nul 2>&1
+IF ERRORLEVEL 1 EXIT /B 1
+EXIT /B 0
+
+:SetWindowsSdkVersionHelper
+SET WindowsSdkVersion=
+FOR /F "tokens=1,2*" %%i in ('REG QUERY "%1\SOFTWARE\Microsoft\Microsoft SDKs\Windows" /V "CurrentVersion"') DO (
+    IF "%%i"=="CurrentVersion" (
+        SET "WindowsSdkVersion=%%k"
+    )
+)
+IF "%WindowsSdkVersion%"=="" EXIT /B 1
+EXIT /B 0
+
+:ERROR_NOWSDK
+ECHO The Windows SDK %WindowsSdkVersion% could not be found.
+EXIT /B 1
+
+:END
